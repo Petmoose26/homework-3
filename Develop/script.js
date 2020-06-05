aa66f49aa5e70702cf5d601340af3d8b13457823
james louis <jlouis1026@gmail.com>
2020-06-05 19:13:44 -0400

    revisited javaScript after seeking assistance from tutor

--- Develop/script.js
+++ Develop/script.js
@@ -1,89 +1,53 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var LowerCase = "abcdefghijklmnopqrstuvwxyz";
-var SpecialChar = "!@#$%^&*()_+=-?.,";
-var numbers = "0123456789";
-
-
-
-// Write password to the #password input
-function writePassword() {
-var password = generatePassword();
-var passwordText = document.querySelector("#password");
-passwordText.value = password;
-};
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Assignment Code
-
-
-var UserChoice = prompt("How long would you like your password to be? Choose between 8 and 128 characters");
-var SpecialChar = confirm("Would you like special characters?");
-var numbers = confirm("Would you like numbers?");
-var UpperCase = confirm("Would you like uppercase letters?");
-var LowerCase = confirm("would you like lowercase letters?");
-var passW = "";
-
-if (UserChoice < 8) {
-  var UserChoice = 8;
+// display password in text area.
+function displayRandomPassword() {
+  var password = generatePassword();
+  var textArea = document.getElementById('password');
+  textArea.value = password;
 }
-if (UserChoice > 128) {
-  var UserChoice = 128;
-}
-generateBtn.addEventListener("click", writePassword); {
 
-  for (var i = 0; i < UserChoice; i++) {
-    if (SpecialChar === true && numbers === true && UpperCase === true && LowerCase ===true) {
-      var password = (UpperCase + LowerCase + numbers + SpecialChar).charAt(Math.floor(Math.random())) * (UpperCase + LowerCase + SpecialChar + numbers.length);
-    }
-    if (SpecialChar === true && numbers === true && UpperCase === true && LowerCase === false) {
-      var password = (UpperCase + numbers + SpecialChar).charAt(Math.floor(Math.random())) * (UpperCase + numbers + SpecialChar).length;
-    }
-    if (SpecialChar === true && numbers === true && UpperCase === false && LowerCase === true) {
-      var password = (LowerCase + numbers + SpecialChar).charAt(Math.floor(Math.random()) * (LowerCase + numbers + SpecialChar).length);    
-    }
-    if (SpecialChar === true && numbers === false && UpperCase === true && LowerCase === true) {
-      var password = (SpecialChar + LowerCase + UpperCase).charAt(Math.floor(Math.random()) * (SpecialChar + UpperCase + LowerCase).length);
-    }
-    if (SpecialChar === false && numbers === true && UpperCase === true && LowerCase === true) {
-      var password = (numbers + UpperCase + LowerCase).charAt(Math.floor(Math.random()) * (numbers + UpperCase + LowerCase).length);
-    }
-    if (SpecialChar === false && numbers === false && UpperCase === true && LowerCase === true) {
-     var password = (UpperCase + LowerCase).charAt(Math.floor(Math.random()) * (UpperCase + LowerCase).length);
-    }
-    if (SpecialChar === true && numbers === true && UpperCase === false && LowerCase === false) {
-      var password = (SpecialChar + numbers).charAt(Math.floor(Math.random()) * (SpecialChar + numbers).length);
-    }
-    if (SpecialChar === true && numbers === false && UpperCase === true && LowerCase === false) {
-      var password = (SpecialChar + UpperCase).charAt(Math.floor(Math.random()) * (SpecialChar + UpperCase).length);
-    }
-    if (SpecialChar === true && numbers === false && UpperCase === false && LowerCase === true) {
-     var password = (SpecialChar + LowerCase).charAt(Math.floor(Math.random()) * (SpecialChar + LowerCase).length);
-    } 
-    if (SpecialChar === false && numbers === true && UpperCase === true && LowerCase === false) {
-     var password = (numbers + UpperCase).charAt(Math.floor(Math.random()) * (numbers + UpperCase).length);
-    }
-    if (SpecialChar === false && numbers === true && UpperCase === false && LowerCase === true) {
-     var password = (numbers + LowerCase).charAt(Math.floor(Math.random()) * (numbers + LowerCase).length);
-    }
-    if (SpecialChar === false && numbers === false && UpperCase === true && LowerCase === true) {
-     var password = (UpperCase + LowerCase).charAt(Math.floor(Math.random()) * (UpperCase + LowerCase).length);
-    }
-    if (SpecialChar === true && numbers === false && UpperCase === false && LowerCase === false) {
-     var password = (SpecialChar).charAt(Math.floor(Math.random()) * (SpecialChar).length);
-    }
-    if (SpecialChar === false && numbers === true && UpperCase === false && LowerCase === false) {
-     var password = (numbers).charAt(Math.floor(Math.random()) * (numbers).length);
-    }
-    if (SpecialChar === false && numbers === false && UpperCase === true && LowerCase === false) {
-     var password = (UpperCase).charAt(Math.floor(Math.random()) * (UpperCase).length);
-    }
-    if (SpecialChar === false && numbers === false && UpperCase === false && LowerCase === true) {
-      var password = (LowerCase).charAt(Math.floor(Math.random()) * (LowerCase).length);
-    }
-     passW = password
+//generate password after button is pushed  by adding event listener to button.
+var generatePasswordButton = document.getElementById('generate');
+generatePasswordButton.addEventListener('click', displayRandomPassword);
+
+//get user input to help generate password
+function generatePassword() {
+  var userLength = prompt('choose how long you would like your password to be 8 through 128');
+  var upperCase = confirm('would you like upper case letters?');
+  var lowerCase = confirm('would you like lower case letters?');
+  var specialCharacters = confirm('would you like special characters?');
+  var numbers = confirm('would you like numbers?');
+  var password = generatePasswordFromCriteria (upperCase, lowerCase, specialCharacters, numbers, userLength);
+  return password
+}
+// give var's strings, look to see what is true from user response. 
+function generatePasswordFromCriteria(upperCase, lowerCase, specialCharacters, numbers, userLength) {
+  var upperCase = ('QWERTYUIOPASDFGHJKLZXCVBNM');
+  var lowerCase = (upperCase.toLowerCase());
+  var specialCharacters = ('!@#$%^&*()+?');
+  var numbers = ('1234567890');
+  var includedCharacters = "";
+  password = "";
+  //checking if password is to include what the user agreed to. 
+  if (upperCase) {
+    includedCharacters += upperCase;
   }
+  if (lowerCase) {
+    includedCharacters += lowerCase;
+  }
+  if (specialCharacters) {
+    includedCharacters += specialCharacters;
+  }
+  if (numbers) {
+    includedCharacters += numbers;
+  }
+  //running a for loop to run though the strings
+  for (var i = 0; i < userLength; i++) {
+    //random position from  included characters 
+    var randomIndex = Math.floor(Math.random() * includedCharacters.length);
+    //use the random position to find random character. 
+    var randomChar = includedCharacters.charAt(randomIndex);
+    password += randomChar
+  }
+  return password;
 }
+ 
